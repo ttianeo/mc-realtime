f06613e1436885925f2f8f8a387fca32a895a5e8
ttianeo <cbpantianya@163.com>
2025-01-26 18:57:29 +0800

    feat(fabric-api): upgrade fabric version and use new api
    effected version: 1.20-1.21.4

--- src/main/java/com/tianeo/realtime/realtime/Realtime.java
+++ src/main/java/com/tianeo/realtime/realtime/Realtime.java
@@ -1,12 +1,11 @@
 package com.tianeo.realtime.realtime;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.server.ServerStartCallback;
-import net.fabricmc.fabric.api.event.server.ServerTickCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,18 +13,18 @@ import java.time.LocalTime;
 
 public class Realtime implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("Realtime");
-    public int SetTimeOfDayCount = 59;
+    public int SetTimeOfDayCount = 0;
 
     @Override
     public void onInitialize() {
         // Register GameRules Event
-        ServerStartCallback.EVENT.register(this::DisableDoDaylightCycle);
-        ServerTickCallback.EVENT.register(this::SetTimeToRealtime);
+        ServerWorldEvents.LOAD.register(this::DisableDoDaylightCycle);
+        ServerTickEvents.START_SERVER_TICK.register(this::SetTimeToRealtime);
         LOGGER.info("Successfully loaded Realtime");
     }
 
     // disable doDaylightCycle and init the value of timeOfDay
-    public void DisableDoDaylightCycle(MinecraftServer server) {
+    public void DisableDoDaylightCycle(MinecraftServer server, ServerWorld _w) {
         Iterable<ServerWorld> worlds = server.getWorlds();
         worlds.forEach(world -> {
             GameRules rules = world.getGameRules();
@@ -38,19 +37,27 @@ public class Realtime implements ModInitializer {
     // set game time to real time (every 60 ticks)
     public void SetTimeToRealtime(MinecraftServer server) {
         if (++SetTimeOfDayCount % 60 == 0) {
-            ServerWorld overworld = server.getWorld(World.OVERWORLD);
-            if (overworld != null) {
-                LocalTime systemTime = LocalTime.now(); // system time
-                // 6:00-0tick,18:00-12000tick
-                // seconds passed from 6:00
-                long secondsFromSixAM = systemTime.getSecond() + systemTime.getMinute() * 60 + (systemTime.getHour() - 6) * 3600;
-                // transfer to ticks
-                long ticks = (Math.round(secondsFromSixAM * 24000 / 86400.0) + 24000) % 24000;
-                overworld.method_29199(ticks); // setTimeOfDay
-                LOGGER.info("Set time of day: {}", overworld.getTimeOfDay());
-            } else {
-                LOGGER.error("Failed to get overworld pointer");
+            try {
+                ServerWorld world = server.getOverworld().toServerWorld();
+                if (world != null) {
+                    LocalTime systemTime = LocalTime.now(); // system time
+                    // 6:00-0tick,18:00-12000tick
+                    // seconds passed from 6:00
+                    long secondsFromSixAM = systemTime.getSecond() + systemTime.getMinute() * 60 + (systemTime.getHour() - 6) * 3600;
+                    // transfer to ticks
+                    long ticks = (Math.round(secondsFromSixAM * 24000 / 86400.0) + 24000) % 24000;
+
+                    world.setTimeOfDay(ticks); // setTimeOfDay
+                    LOGGER.info("Target time of day: {}", ticks);
+                    LOGGER.info("Set time of day: {}", world.getTimeOfDay());
+                } else {
+                    throw new Error("Failed to get overworld pointer");
+                }
+            } catch (Exception e) {
+                LOGGER.error("Failed to set time of day", e);
             }
+
+
             SetTimeOfDayCount = 0;
         }
 
